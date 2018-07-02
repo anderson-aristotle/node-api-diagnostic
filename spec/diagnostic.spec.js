31aa11154751c7a8413e2735c422383df90fb6da
cpearce31 <cpearce31@gmail.com>
2018-07-02 16:04:57 -0400

    Simplify diagnostic requirements
    Remove the need to invoke an unseen callback and instantiate the `Error` class.
    
    Close #307.

--- spec/diagnostic.spec.js
+++ spec/diagnostic.spec.js
@@ -1,39 +1,44 @@
 'use strict'
 
-const diagnostic = require('../lib/diagnostic')
+const chai = require('chai')
 
-const createCallback = function (expectedSum, done) {
-  return function (error, sum) {
-    if (error || sum !== expectedSum) {
-      error = error || new Error(`sum is ${sum}`)
-    }
-
-    done(error)
-  }
-}
+const expect = chai.expect
+const diagnosticResponse = require('../lib/diagnostic')
 
 describe('Sum of lines', function () {
   it('for integers.txt, should equal 88', function (done) {
-    diagnostic.sumLines('data/integers.txt', createCallback(88, done))
+    const diagnostic = Object.assign({}, diagnosticResponse)
+    diagnostic.sumLines('data/integers.txt')
+    setTimeout(() => {
+      expect(diagnostic.sum).to.eq(88)
+      done()
+    }, 300)
   })
 
   it('for blanks.txt, should equal 12', function (done) {
-    diagnostic.sumLines('data/blanks.txt', createCallback(12, done))
+    const diagnostic = Object.assign({}, diagnosticResponse)
+    diagnostic.sumLines('data/blanks.txt')
+    setTimeout(() => {
+      expect(diagnostic.sum).to.eq(12)
+      done()
+    }, 300)
   })
 
   it('for numbers.txt, should equal 12.93', function (done) {
-    diagnostic.sumLines('data/numbers.txt', createCallback(12.93, done))
+    const diagnostic = Object.assign({}, diagnosticResponse)
+    diagnostic.sumLines('data/numbers.txt')
+    setTimeout(() => {
+      expect(diagnostic.sum).to.eq(12.93)
+      done()
+    }, 300)
   })
-})
 
-describe('Sum of lines in strings.txt', function () {
-  it('should pass an error', function (done) {
-    diagnostic.sumLines('data/strings.txt', function (error) {
-      if (error instanceof Error) {
-        return done()
-      }
-
-      done(new Error(`${error}`))
-    })
+  it('for strings.txt, should equal null', function (done) {
+    const diagnostic = Object.assign({}, diagnosticResponse)
+    diagnostic.sumLines('data/strings.txt')
+    setTimeout(() => {
+      expect(diagnostic.sum).to.eq(null)
+      done()
+    }, 300)
   })
 })
